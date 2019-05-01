e8c72895e73a480d2bfdcae46269d72254412461
Yoshihide Shiono <shiono.yoshihide@gmail.com>
2019-05-02 04:45:02 +0900

    Remove rimraf and improve dev command

--- dev.js
+++ dev.js
@@ -1,31 +1,30 @@
-const { existsSync } = require('fs')
-const { resolve } = require('path')
-const { execSync } = require('child_process')
-const chalk = require('chalk')
+const { remove, existsSync } = require('fs-extra');
+const { resolve } = require('path');
+const { execSync } = require('child_process');
+const chalk = require('chalk');
 
 async function dev() {
-  process.env.NODE_ENV = 'testing'
-
-  let template = 'with-vuetify'
+  let template = 'with-vuetify';
   if (3 <= process.argv.length) {
-    const newTemplate = process.argv[2]
+    const newTemplate = process.argv[2];
     if (!existsSync(resolve(__dirname, `examples/${newTemplate}`))) {
-      console.log(chalk.red(`Not found examples/${newTemplate}`))
-      console.log('')
-      process.exit(1)
+      console.log(chalk.red(`Not found examples/${newTemplate}`));
+      console.log('');
+      process.exit(1);
     }
-    template = newTemplate
+    template = newTemplate;
   }
 
+  await remove('workspace');
   execSync('node ' + resolve(__dirname, `bin/nuxtron init workspace --template ${template}`), {
     cwd: __dirname,
-    stdio: 'inherit'
-  })
-  execSync('cd workspace')
+    stdio: 'inherit',
+  });
+  execSync('cd workspace');
   execSync('yarn && yarn dev', {
     cwd: resolve(__dirname, 'workspace'),
-    stdio: 'inherit'
-  })
+    stdio: 'inherit',
+  });
 }
 
-dev()
+dev();
